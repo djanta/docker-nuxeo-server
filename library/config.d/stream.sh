5dfabf70e1e2dc122c13377538db0f3d6cb5aa24
djanta.io <github@djanta.io>
2021-01-21 20:12:13 -0500

    Update stream.sh

--- library/config.d/stream.sh
+++ library/config.d/stream.sh
@@ -21,3 +21,12 @@ source "/library/common.sh"
 
 #nuxeo.stream.audit.batch.size=1000
 #nuxeo.stream.audit.batch.threshold.ms=1000
+
+    # Audit log optimization
+    [ -n "$NUXEO_AUDIT_LOG_BATCH_THRESHOLD" ] && echo "nuxeo.stream.audit.batch.threshold.ms=$NUXEO_AUDIT_LOG_BATCH_THRESHOLD" >> "$NUXEO_CONF"
+    [ -n "$NUXEO_AUDIT_LOG_BATCH_SIZE" ] && echo "nuxeo.stream.audit.batch.size=$NUXEO_AUDIT_LOG_BATCH_SIZE" >> "$NUXEO_CONF"
+
+    # Bulk Stream configuration
+    [ -n "$NUXEO_BULK_DONE_CONCURRENCY_MAX" ] && echo "nuxeo.core.bulk.done.concurrencyMax=$NUXEO_BULK_DONE_CONCURRENCY_MAX" >> "$NUXEO_CONF"
+    [ -n "$NUXEO_BULK_SCROLL_CONCURRENCY_MAX" ] && echo "nuxeo.core.bulk.scroller.concurrencyMax=$NUXEO_BULK_SCROLL_CONCURRENCY_MAX" >> "$NUXEO_CONF"
+    [ -n "$NUXEO_BULK_STATUS_CONCURRENCY_MAX" ] && echo "nuxeo.core.bulk.status.concurrencyMax=$NUXEO_BULK_STATUS_CONCURRENCY_MAX" >> "$NUXEO_CONF"
